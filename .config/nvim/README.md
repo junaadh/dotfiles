d7d83d0f85fc8bd238ae82bb043d5fea845435d6
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-12 02:22:54 +0500

    added lsp

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -1,3 +1,6 @@
 # Neovim config using Lazy
 
+- helix inspired neovim
+- most common keyaps used in helix migrated
+
 *WORK IN PROGRESS*
--- .config/nvim/lua/conf/remap.lua
+++ .config/nvim/lua/conf/remap.lua
@@ -2,9 +2,10 @@ vim.g.mapleader = " "
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 local nmap = vim.api.nvim_set_keymap
+local vm = vim.g
 
 -- Finder
-map("n", "<leader>ff", ":Telescope find_files<CR><ESC>", opts)
+map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
 map("n", "<leader>fm", ":Telescope man_pages<CR>", opts)
 
@@ -38,3 +39,19 @@ nmap("n", "U", "<C-r>", opts)
 
 nmap('n', '<C-S-Up>', ":m-2<CR>:normal! kj<CR>", opts)
 nmap('n', '<C-S-Down>', ":m+<CR>:normal! jk<CR>", opts)
+
+-- Visual multi
+vm.VM_leader = {
+  default = '<S>',
+  visual = '<S>',
+  buffer = 'b',
+}
+vm.VM_maps = {}
+vm.VM_maps["Get Operator"] = '<S>a'
+vm.VM_maps["Add Cursor At Pos"] = '<Leader><Space>'
+vm.VM_maps["Reselect Last"] = '<S>z'
+vm.VM_maps["Visual Cursors"] = '<S><Space>'
+vm.VM_maps["Undo"] = 'u'
+vm.VM_maps["Redo"] = 'U'
+vm.VM_maps["Visual Subtract"] = 'bs'
+vm.VM_maps["Visual Reduce"] = 'br'
