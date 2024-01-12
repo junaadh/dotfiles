63e397f8a08a539a7353271aefed6ac14a80ffa9
Moosa Junaadh <junaadh.02@gmail.com>
2024-01-13 00:24:09 +0500

    custom commenting

---
+++ .config/nvim/lua/plugins/cursors.lua
@@ -0,0 +1,9 @@
+return {
+  "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'smoka7/hydra.nvim',
+    },
+  cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+  opts = {},
+}
