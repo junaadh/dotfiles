d4af459fd78c0e176be95a2fceba53b7a04c38c0
Junaadh <junaadh.02@gmail.com>
2024-04-17 03:11:12 +0800

    periodic update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,13 +6,16 @@ end
 set -gx PATH /opt/homebrew/opt/binutils/bin $PATH
 set -gx PATH /opt/homebrew//bin $PATH
 set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/Library/Python/3.9/bin $PATH
 set -gx PATH_TO_FX $HOME/.sdk/javafx-sdk-21.0.1/lib
-set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home
+# set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home
 set -gx FDOTS $HOME/.config/fish
 set -Ua fish_user_paths $HOME/.cargo/bin
 set -gx BDT $HOME/Documents/Acadamics/sem3.5/BDT
 set -gx CCDE $HOME/Documents/Acadamics/sem3.5/CCDE
 set -gx clientid 1208484529510154260
+set -gx DISCORD_APP_ID 996864734957670452
+set -gx PYSPARK_PYTHON /opt/homebrew//bin/python3
 
 set -g fish_follow_symlinks 1
 
@@ -24,6 +27,8 @@ alias ls="eza --icons"
 alias refresh="source $FDOTS/config.fish"
 alias cls="clear"
 alias hxf="hx $FDOTS"
+
+alias tree="eza --icons -la -T --level 3 --git-ignore"
 
 # git
 alias gs="git status"
@@ -39,5 +44,13 @@ alias clean="brew autoremove && brew cleanup"
 alias list="brew list"
 alias update="brew update && brew upgrade"
 
-~/Developer/rust/rise_code/scripts/rise_code_startup.sh
+alias awk="gawk"
+
 starship init fish | source
+~/.cargo/rise_code/launcher.sh
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+ source /Users/junaadh/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
